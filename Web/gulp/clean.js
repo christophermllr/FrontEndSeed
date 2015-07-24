baec226ab9b8e8cd927c0e3f09c5abaeef060075
jmongiat <jmongiat@live.com>
2015-07-23 20:21:50 -0500

    fixing paths

--- Web/gulp/clean.js
+++ Web/gulp/clean.js
@@ -30,6 +30,12 @@ gulp.task('clean-dev', ['clean'], function () {
     $.del.sync(delPaths);
 });
 gulp.task('clean-dist', ['clean'], function () {
-    $.del.sync([
-    ]);
+    
+    var delPaths = [];
+
+    for(var key in config.paths.clean)
+    {
+        delPaths.push(path.join(config.root, config.paths.output.dist, config.paths.clean[key]));
+    }
+    $.del.sync(delPaths);
 });
