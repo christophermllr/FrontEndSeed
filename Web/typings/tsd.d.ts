261bc9fdf3f828ee39587d789e38a56d06851665
jmongiat <jmongiat@westmonroepartners.com>
2015-07-24 15:45:06 -0500

    more fixes

--- Web/typings/tsd.d.ts
+++ Web/typings/tsd.d.ts
@@ -1,10 +1,10 @@
 ///{
 /// <reference path="../src/main.ts" />
-/// <reference path="../src/module-b/module-b-index-controller.ts" />
-/// <reference path="../src/module-b/module-b.ts" />
 /// <reference path="../src/module-a/IDemoModel.ts" />
 /// <reference path="../src/module-a/module-a-demo-controller.ts" />
 /// <reference path="../src/module-a/module-a-module.ts" />
+/// <reference path="../src/module-b/module-b-index-controller.ts" />
+/// <reference path="../src/module-b/module-b.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 //}
